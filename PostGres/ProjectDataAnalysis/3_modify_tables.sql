24bee5a89b41b89a93d588e5265ee6eed4949809
sraza99 <sraza99@gmail.com>
2025-04-18 00:32:52 -0500

    commit changes

---
+++ PostGres/ProjectDataAnalysis/3_modify_tables.sql
@@ -0,0 +1,57 @@
+/* ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
+Database Load Issues (follow if receiving permission denied when running SQL code below)
+
+One of the errors I had was on read permission denied for csv file. So I went to file explorer
+and right-clicked the file and selected properties. Then I went to security tab and clicked add. I then
+entered Everyone is dialog box and clicked ok. clicked ok to save settings again. And then the 
+script ran successfully. 
+
+Possible Errors: 
+- ERROR >>  duplicate key value violates unique constraint "company_dim_pkey"
+- ERROR >> could not open file "C:\Users\...\company_dim.csv" for reading: Permission denied
+
+1. Drop the Database 
+            DROP DATABASE IF EXISTS sql_course;
+2. Repeat steps to create database and load table schemas
+            - 1_create_database.sql
+            - 2_create_tables.sql
+3. Open pgAdmin
+4. In Object Explorer (left-hand pane), navigate to `sql_course` database
+5. Right-click `sql_course` and select `PSQL Tool`
+            - This opens a terminal window to write the following code
+6. Get the absolute file path of your csv files
+            1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
+7. Paste the following into `PSQL Tool`, (with the CORRECT file path)
+
+\copy company_dim FROM '[Insert File Path]/company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+\copy skills_dim FROM '[Insert File Path]/skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+\copy job_postings_fact FROM '[Insert File Path]/job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+\copy skills_job_dim FROM '[Insert File Path]/skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+*/
+
+-- NOTE: This has been updated from the video to fix issues with encoding
+/*In video this script went something like this:
+COPY company_dim
+FROM 'C:\Users\sraza\VisualCodeEditor\PostGres\ProjectDataAnalysis\company_dim.csv'
+DELIMITER ',' CSV HEADER; */
+
+COPY company_dim
+FROM 'C:\Users\sraza\VisualCodeEditor\PostGres\ProjectDataAnalysis\company_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_dim
+FROM 'C:\Users\sraza\VisualCodeEditor\PostGres\ProjectDataAnalysis\skills_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY job_postings_fact
+FROM 'C:\Users\sraza\VisualCodeEditor\PostGres\ProjectDataAnalysis\job_postings_fact.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_job_dim
+FROM 'C:\Users\sraza\VisualCodeEditor\PostGres\ProjectDataAnalysis\skills_job_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
